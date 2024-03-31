87b486fb772122c905d8eb44dc440fa5405f1ee2
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-03-31 17:45:30 -0300

    chore: add export for utils

---
+++ lib/core/util/utils.dart
@@ -0,0 +1,8 @@
+export 'constants/movie_colors.dart';
+export 'constants/movie_words.dart';
+export 'constants/assets_path.dart';
+export 'constants/routes.dart';
+export 'graphql_mutations.dart';
+export 'graphql_querys.dart';
+export 'status_enum.dart';
+export 'usecase.dart';
