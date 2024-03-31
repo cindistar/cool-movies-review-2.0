6d2014f7c00d8bad8328662d546f358faef0528e
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-03-31 18:08:07 -0300

    feat: add movies list state

---
+++ lib/presentation/movies_list/bloc/movies_list/movies_list_state.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:coolmovies/core/util/status_enum.dart';
+import 'package:coolmovies/domain/entities/movies/movies_entity.dart';
+import 'package:equatable/equatable.dart';
+
+class MoviesListState extends Equatable {
+  final List<MoviesEntity>? moviesList;
+  final StatusEnum status;
+  const MoviesListState({
+    this.moviesList,
+    this.status = StatusEnum.initial,
+  });
+
+  @override
+  List<Object?> get props => [status, moviesList];
+
+  MoviesListState copyWith({
+    List<MoviesEntity>? moviesList,
+    StatusEnum? status,
+  }) {
+    return MoviesListState(
+      moviesList: moviesList ?? this.moviesList,
+      status: status ?? this.status,
+    );
+  }
+}
