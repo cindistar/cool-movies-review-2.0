2c2f9225be572a2c6c638ab9d3d4f49d12e7efdd
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-03-31 17:54:43 -0300

    chore: add export for usecases

---
+++ lib/domain/usecases/usecases.dart
@@ -0,0 +1,6 @@
+export 'movies/get_all_movies_list_usecase.dart';
+export 'review/create_review_usecase.dart';
+export 'review/delete_review_usecase.dart';
+export 'review/get_all_movie_reviews_usecase.dart';
+
+
