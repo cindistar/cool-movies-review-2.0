24153f287ae2f61457be17825c759143e8a67bb6
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-03-31 18:05:57 -0300

    chore: add export for bloc

---
+++ lib/presentation/movie_details/bloc/delete_review/bloc.dart
@@ -0,0 +1,3 @@
+export 'delete_review_bloc.dart';
+export 'delete_review_event.dart';
+export 'delete_review_state.dart';
---
+++ lib/presentation/movie_details/bloc/reviews_list/bloc.dart
@@ -0,0 +1,3 @@
+export 'reviews_list_bloc.dart';
+export 'reviews_list_event.dart';
+export 'reviews_list_state.dart';
