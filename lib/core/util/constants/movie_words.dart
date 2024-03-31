e1e9bfb5f5010be63ffc62f047c2e22dc1f5c80a
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-03-31 17:48:35 -0300

    chore: add movie words class to concentrate the words and sentences used in the app in one place

---
+++ lib/core/util/constants/movie_words.dart
@@ -0,0 +1,30 @@
+class MovieWords {
+  static String createReview = "Create a Review";
+  static String addReviewForMovie = "Add a review for the movie ";
+  static String id = "Id";
+  static String pleaseEnterId = "Please enter an Id";
+  static String title = "Title";
+  static String pleaseEnterTitle = "Please enter a title";
+  static String reviewText = "Review Text";
+  static String pleaseEnterReview = "Please type a review";
+  static String ratingoneToFive = "Rating - 1 to 5";
+  static String pleaseTypeRating = "Please type a rating value";
+  static String reviewerId = "Reviewer Id";
+  static String pleaseEnterReviewerId = "Please enter a reviewer Id";
+  static String submitReview = "Submit review";
+  static String close = "Close";
+  static String reviewDeletedSuccess = "Review deleted!";
+  static String genericErrorMessage = "Something went wrong. Try again!";
+  static String createdReviewSuccess = "Review created!";
+  static String movieReviewsDetails = "Movie's Reviews and Details";
+  static String releaseDate = "Release date:";
+  static String reviews = "Reviews";
+  static String addReview = "Add a review";
+  static String review = "Review";
+  static String rating = "Rating";
+  static String swipeLeftDeleteReview = "Swipe left to delete this review";
+  static String movies = "Movies";
+  static String coolMovies = "Cool Movies";
+  static String validatorOnlyNumbersAllowed =
+      "Only integer numbers from 1 to 5 are allowed";
+}
