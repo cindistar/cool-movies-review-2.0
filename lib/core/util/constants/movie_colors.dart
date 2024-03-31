baab4b6c760b3e953332521ac1f7cc414bb9ab82
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-03-31 17:47:33 -0300

    chore: add movie color class to concentrate the main colors used in the app in one place

---
+++ lib/core/util/constants/movie_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MovieColors {
+  static const greenApple = Color(0xfff5f378);
+  static const lightGreenApple = Color.fromARGB(255, 238, 237, 187);
+  static const lilacGarden = Color(0xffddc1ff);
+  static const redMaple = Color(0xffec704b);
+}
