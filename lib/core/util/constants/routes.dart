4331ce750dc4af14b323b1c9f8f504c732e667c7
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-03-31 17:49:11 -0300

    chore: add routes class to concentrate the routes used in the app in one place

---
+++ lib/core/util/constants/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const String splash = '/splash';
+  static const String moviesList = '/movies-list';
+  static const String movieDetails = '/movie-details';
+  static const String reviews = '/reviews';
+}
