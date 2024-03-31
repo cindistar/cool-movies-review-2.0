182e36cd0642cf5abcba7069ac4fbcccfca6f33a
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-03-31 18:27:24 -0300

    test: delete event bloc - unit / bloc test

---
+++ test/presentation/movie_details/bloc/delete_review/delete_review_event_test.dart
@@ -0,0 +1,21 @@
+import 'package:coolmovies/domain/entities/entities.dart';
+import 'package:coolmovies/presentation/movie_details/bloc/delete_review/bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../mocks/review_mock.dart';
+
+void main() {
+  ReviewEntity review = ReviewMock.reviewEntity;
+  String reviewId = review.id;
+
+   test('verify DeleteReview is DeleteReviewEvent', () {
+    expect(DeleteReview(reviewId: reviewId),
+        isA<DeleteReviewEvent>());
+  });
+
+  test('verify DeleteReview is equal to Equatable props', () {
+    DeleteReview instance =
+       DeleteReview(reviewId: reviewId);
+    expect(instance.props.isNotEmpty, true);
+  });
+}
